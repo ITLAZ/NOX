3b6ef28099581e5e267a33972492e1ae8ff2e293
GabrielFernandoFloresSantaCruz <gabriel.flores.s@ucb.edu.bo>
2025-05-15 12:33:19 -0400

    Weas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -53,7 +53,15 @@ const routes: Routes = [
   {
     path: '**',
     redirectTo: '/home',
+  },  {
+    path: 'cart',
+    loadChildren: () => import('./pages/cart/cart.module').then( m => m.CartPageModule)
   },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+
 ];
 
 @NgModule({
