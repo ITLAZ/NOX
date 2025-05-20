563e012c92ee79ff9829b04ab6f36800d144735f
ITLAZ <juan.tapia.l@ucb.edu.bo>
2025-05-19 20:19:42 -0400

    chips filtros

---
+++ src/app/components/filter-actions/filter-actions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'filter-actions',
+  templateUrl: './filter-actions.component.html',
+  styleUrls: ['./filter-actions.component.scss'],
+  standalone: false,
+})
+export class FilterActionsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
