6be59f65c698bef29b760dbdd44d32a05a0f26fd
Ceci-F <158775879+Ceci-F@users.noreply.github.com>
2025-05-20 22:00:22 -0400

    mapa-card

---
+++ src/app/components/cards/mapa-card/mapa-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mapa-card',
+  templateUrl: './mapa-card.component.html',
+  styleUrls: ['./mapa-card.component.scss'],
+  standalone: false
+})
+export class MapaCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
