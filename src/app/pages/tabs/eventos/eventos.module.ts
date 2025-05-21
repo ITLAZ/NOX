f57fd52d4967c5e6e6a72fdd45dc95091d6d7d02
ITLAZ <juan.tapia.l@ucb.edu.bo>
2025-05-21 19:55:26 -0400

    cosas varias y toolbar

--- src/app/pages/tabs/eventos/eventos.module.ts
+++ src/app/pages/tabs/eventos/eventos.module.ts
@@ -1,17 +1,16 @@
 import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { EventosPageRoutingModule } from './eventos-routing.module';
-
 import { EventosPage } from './eventos.page';
+
 import { MidCardEventoModule } from 'src/app/shared/mid-card-evento/mid-card-evento.module';
 import { SearchModule } from 'src/app/shared/search/search.module';
 import { FilterModalModule } from 'src/app/shared/filter-modal/filter-modal.module';
 import { ActionCardModule } from 'src/app/shared/action-card/action-card.module';
-
+import { ToolbarModule } from 'src/app/shared/toolbar/toolbar.module';
 import { MediaCardEventoModule } from 'src/app/shared/media-card-evento/media-card-evento.module';
 
 
@@ -25,8 +24,8 @@ import { MediaCardEventoModule } from 'src/app/shared/media-card-evento/media-ca
     SearchModule,
     FilterModalModule,
     ActionCardModule,
-    MediaCardEventoModule
-  
+    MediaCardEventoModule,
+    ToolbarModule
   ],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   declarations: [EventosPage]
