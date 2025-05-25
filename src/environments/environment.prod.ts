5ac1fb234bec49e802534e8561c4218caa4adc8c
ITLAZ <juan.tapia.l@ucb.edu.bo>
2025-05-24 23:46:30 -0400

    Nunca senti tanto miedo en mi vida

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDpWCqkVAApb3oArK1WGBiLMVnYSrBq-YQ",
+    authDomain: "nox-app-tdig.firebaseapp.com",
+    projectId: "nox-app-tdig",
+    storageBucket: "nox-app-tdig.firebasestorage.app",
+    messagingSenderId: "445585900604",
+    appId: "1:445585900604:web:93619862b015411aecaf39",
+    measurementId: "G-X2FJV9JHDT"
+  }
 };
