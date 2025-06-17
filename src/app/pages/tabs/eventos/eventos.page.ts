0e9a20e5e7bd72f65dc50e73cb4ab1164873c2cd
Ceci-F <158775879+Ceci-F@users.noreply.github.com>
2025-06-17 00:01:00 -0400

    eventos responsive

--- src/app/pages/tabs/eventos/eventos.page.ts
+++ src/app/pages/tabs/eventos/eventos.page.ts
@@ -14,44 +14,48 @@ export class EventosPage implements OnInit {
   events: any[] = [];
   allEvents: any[] = [];
   isLoading = true;
+  isWeb = window.innerWidth >= 769;
 
   constructor(private db: DatabaseService, private router: Router) {}
 
 
-ngOnInit() {
-  this.db.getEvents().subscribe(data => {
-    console.log('Eventos cargados directamente:', data);
-    this.allEvents = data;
-    this.events = data;
-    this.isLoading = false;
-  });
-}
-
-onEtiquetaSeleccionada(etiqueta: string) {
-  if (!etiqueta) {
-    this.events = this.allEvents;
-    return;
-  }
-  this.events = this.allEvents.filter(evento => (evento.etiquetas || []).includes(etiqueta));
-}
-
-irAEvento(evento: any) {
-    this.router.navigate(['/event-detail'], {
-      queryParams: { id: evento.id }
+  ngOnInit() {
+    window.addEventListener('resize', () => {
+      this.isWeb = window.innerWidth >= 769;
+    });
+    this.db.getEvents().subscribe(data => {
+      console.log('Eventos cargados directamente:', data);
+      this.allEvents = data;
+      this.events = data;
+      this.isLoading = false;
     });
   }
-formatFecha(fechaTimestamp: any): string {
-  if (!fechaTimestamp) return 'Fecha no disponible';
 
-  // Convierte el timestamp de Firebase si es necesario
-  if (fechaTimestamp.toDate) {
-    fechaTimestamp = fechaTimestamp.toDate();
+  onEtiquetaSeleccionada(etiqueta: string) {
+    if (!etiqueta) {
+      this.events = this.allEvents;
+      return;
+    }
+    this.events = this.allEvents.filter(evento => (evento.etiquetas || []).includes(etiqueta));
   }
 
-  return fechaTimestamp.toLocaleDateString('es-ES', {
-    day: '2-digit',
-    month: 'long',
-    year: 'numeric'
-  });
-}
+  irAEvento(evento: any) {
+      this.router.navigate(['/event-detail'], {
+        queryParams: { id: evento.id }
+      });
+    }
+  formatFecha(fechaTimestamp: any): string {
+    if (!fechaTimestamp) return 'Fecha no disponible';
+
+    // Convierte el timestamp de Firebase si es necesario
+    if (fechaTimestamp.toDate) {
+      fechaTimestamp = fechaTimestamp.toDate();
+    }
+
+    return fechaTimestamp.toLocaleDateString('es-ES', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    });
+  }
 }
